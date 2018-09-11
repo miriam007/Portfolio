b5947d4c8692fc012f73229befc922b24aaa09c2
miriam007 <miriamu88@gmail.com>
2018-09-11 18:16:33 -0500

    updates to css

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ class App extends Component {
         name='Miriam Yazdanpanahi'
         image='https://cdn.pixabay.com/photo/2018/08/15/09/52/programmer-3607627_960_720.png'
         />
-        <div className='projectHeading'>Projects</div>
         <Project 
         title='Project 1'
         image='https://cdn.pixabay.com/photo/2016/03/26/13/09/cup-of-coffee-1280537_960_720.jpg'
         text='This is the description of project 1.'
+        //link=''//
         />
         <Project 
         title='Project 2'
-        image='https://cdn.pixabay.com/photo/2017/08/25/13/36/code-geek-2680204_960_720.png'
+        image='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
         text='This is the decription of project 2.'
+        //link=''//
         />
+        <div className='contact'>Contact Me</div>
       </div>
     );
   }
--- src/Components/githubProjects.js
+++ src/Components/githubProjects.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 class Project extends Component {
     render() {
       return (
-        <div>
-          <h1>{this.props.title}</h1>
+        <div className="projects">
+          <h1 className="projectTitle">{this.props.title}</h1>
             <div className="projectPic">
                 <img alt="project" src={this.props.image}></img>
             </div>
           <div className="projectText">{this.props.text}</div>
+          <div>{this.props.link}</div>
         </div>
       );
     }
